18de04ccced6844059403fba0411e076460232b6
ShantnuS <shantnu@outlook.com>
2018-08-15 09:49:25 +0100

    security groups with interdependencies are now deleted

--- clean-security-groups.py
+++ clean-security-groups.py
@@ -5,10 +5,28 @@ print("AWSMan - Cleaning Security Groups!")
 print("----------------------------------")
 
 #Get the list of all security groups and instances
-ec2 = boto3.resource('ec2')
+ec2resource = boto3.resource('ec2')
+ec2client = boto3.client('ec2')
 
-instances = list(ec2.instances.all())
-sgs = ec2.security_groups.all()
+instances = list(ec2resource.instances.all())
+sgs = ec2resource.security_groups.all()
+
+#Delete a particular sg 
+def delete_sg(ec2, sg_id):
+    print("Deleting: " + sg_id)
+    return ec2.delete_security_group(GroupId=sg_id)
+
+#Remove sg's rules so it can be deleted 
+def remove_sg_rules(ec2resource, ec2client, sg_id):
+    print("Revoking rules for: " + sg_id)
+    sg = ec2resource.SecurityGroup(sg_id)
+    #Removes all rules from that security group and adds them to a list 
+    try:
+        response = sg.revoke_ingress(IpPermissions=sg.ip_permissions)
+    except:
+        print("Continuing...")
+
+    return sg_id
 
 #Make a list of sg ids 
 all_sgs=[]
@@ -23,13 +41,28 @@ for instance in instances:
 
 #Make a list of unused sg ids from all - used
 unused_sgs=[x for x in all_sgs if x not in used_sgs]
+revoked_sgs=[]
 
 #Delete any unused security groups using IDs 
-ec2 = boto3.client('ec2')
 for sg in unused_sgs:
-    print("Deleting: " + sg)
     #Need a try except block since some security groups are referenced by other groups rather than instances 
     try:
-        response = ec2.delete_security_group(GroupId=sg)
+        response = delete_sg(ec2client, sg)
     except:
-        print("Could not delete: " + sg)
+        print("Could not delete: " + sg + ", revoking rules!")
+        sg_id = remove_sg_rules(ec2resource, ec2client, sg)
+        revoked_sgs.append(sg_id)
+
+if not revoked_sgs:
+    print("Revoked list was empty!")
+else:
+    print("Removing from revoked list...")
+    for sg in revoked_sgs:
+        try:
+            response = delete_sg(ec2client, sg)
+        except:
+            print("Could not delete: " + sg)
+
+print("Deleted all groups that could have been deleted")
+print("Remaining groups may be unused but are being referenced by other used groups")
+print("DONE!")
